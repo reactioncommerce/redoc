fd1fbdbc7362edf1fe98d465572f11f361cb0d28
Mike Murray <hellomikemurray@gmail.com>
2016-01-05 21:02:13 -0800

    Pull GA account from settings

--- common/main.jsx
+++ common/main.jsx
@@ -31,6 +31,6 @@ if (Meteor.isClient) {
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-XXXXXXXX-X', 'auto');
+  ga('create', Meteor.settings.public.ga.account, 'auto');
   ga('send', 'pageview');
 }
