920795c3190a606f4dd36f01cf58f7824f9073d1
Mike Murray <hellomikemurray@gmail.com>
2016-01-05 20:54:08 -0800

    Added google analytics example

--- common/main.jsx
+++ common/main.jsx
@@ -1,5 +1,6 @@
 import Layout from "../common/layout.jsx";
 import Docs from "../common/docs/docs.jsx";
+import ReactCookie from "react-cookie";
 
 const {Route, IndexRoute} = ReactRouter;
 
@@ -10,4 +11,26 @@ const AppRoutes = (
   </Route>
 );
 
-ReactRouterSSR.Run(AppRoutes);
+ReactRouterSSR.Run(AppRoutes,{
+  props: {
+    onUpdate() {
+      // Notify the page has been changed to Google Analytics
+      ga('send', 'pageview');
+    }
+  }
+}, {
+  preRender: function(req, res) {
+    ReactCookie.plugToRequest(req, res);
+  }
+});
+
+if (Meteor.isClient) {
+  // Load Google Analytics
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-XXXXXXXX-X', 'auto');
+  ga('send', 'pageview');
+}
