c0e87ae13c53e44379519f7441073a216236286c
aaronjudd <aaron@ongoworks.com>
2016-03-24 13:19:22 -0700

    linting

--- packages/redoc-core/components/avatar.jsx
+++ packages/redoc-core/components/avatar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 class Avatar extends React.Component {
   render() {
     if (this.props.githubUserId) {
-      const imageUrl = `https://avatars.githubusercontent.com/u/${this.props.githubUserId}?s=460`
+      const imageUrl = `https://avatars.githubusercontent.com/u/${this.props.githubUserId}?s=460`;
       return (
         <div className="redoc profile-image">
           <img src={imageUrl} />
